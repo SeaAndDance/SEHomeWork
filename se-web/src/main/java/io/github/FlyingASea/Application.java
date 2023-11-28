56f20714fb697905302506bef9fe9271c4032638
FlyingASea <dth123456.abd@gmail.com>
2023-11-29 00:16:17 +0800

    init change

--- se-web/src/main/java/io/github/FlyingASea/Application.java
+++ se-web/src/main/java/io/github/FlyingASea/Application.java
@@ -1,8 +1,10 @@
-package com.example.sehomework;
+package io.github.FlyingASea;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class Application {
 
